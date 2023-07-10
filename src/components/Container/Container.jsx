96c35959ae50214bf2e1bf479cb9ccec29d0d6b5
Constantine Tereschenkov <kdubrovsky@gmail.com>
2023-07-11 01:31:10 +0400

    Add container component

---
+++ src/components/Container/Container.jsx
@@ -0,0 +1,11 @@
+import css from './Container.module.scss';
+
+export default function Container(
+    { children }
+) {
+    return (
+        <div className={css.container}>
+            {children}
+        </div>
+    )
+}
