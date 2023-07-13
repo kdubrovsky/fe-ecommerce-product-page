a0359b6b41376ce8a38f2b6f20450f807f949954
Constantine Tereschenkov <kdubrovsky@gmail.com>
2023-07-13 23:26:02 +0400

    Add gallery component and dependencies

--- src/components/ProductGallery/ProductGallery.jsx
+++ src/components/ProductGallery/ProductGallery.jsx
@@ -1,24 +1,52 @@
-export default function ProductGallery() {
-    return <p>ProductGallery</p>;
+import { useState } from 'react';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Thumbs } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/thumbs';
+
+import css from "./ProductGallery.module.scss";
+
+export default function ProductGallery({ galleryData }) {
+
+    const [thumbsSwiper, setThumbsSwiper] = useState(null);
+
+    const swiperSlides = galleryData.map(image => (
+        <SwiperSlide
+            className={css.slide}
+            key={image.id}>
+            <img src={image.src} alt={image.alt} />
+        </SwiperSlide>
+    ));
+
+    const swiperThumbnails = galleryData.map(image => (
+        <SwiperSlide
+            className={css.thumbnail}
+            key={image.id}>
+            <img src={image.thumb} alt={image.alt + " Thumbnail"} />
+        </SwiperSlide>
+    ));
+
+    return (
+        <div className={css.gallery}>
+            <Swiper
+                modules={[Thumbs]}
+                thumbs={{ swiper: thumbsSwiper }}
+                spaceBetween={50}
+                slidesPerView={1}
+                className={css.slides}
+            >
+                {swiperSlides}
+            </Swiper>
+            <Swiper
+                onSwiper={setThumbsSwiper}
+                watchSlidesProgress={true}
+                slidesPerView='auto'
+                modules={[Thumbs]}
+                className={css.thumbnails}
+            >
+                {swiperThumbnails}
+            </Swiper>
+        </div >
+    )
 }
-
-
-//     "images": [
-//         {
-//             "id": 0,
-//             "src": "../assets/product/image-product-1.jpg"
-//         },
-//         {
-//             "id": 1,
-//             "src": "../assets/product/image-product-2.jpg"
-//         },
-//         {
-//             "id": 2,
-//             "src": "../assets/product/image-product-3.jpg"
-//         },
-//         {
-//             "id": 3,
-//             "src": "../assets/product/image-product-4.jpg"
-//         }
-//     ]
-// }
